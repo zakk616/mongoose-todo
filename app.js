10f1e4e97446e01625ffa13583dc12993e2c04ba
zakk <muhammadzakaria616@gmail.com>
2022-07-21 21:29:22 +0500

    fisrt commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
