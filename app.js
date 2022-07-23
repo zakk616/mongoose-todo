8f9240bc038f89fba4d1e88c04a76a4de791694b
zakk <muhammadzakaria616@gmail.com>
2022-07-23 18:30:32 +0500

    applied css

--- app.js
+++ app.js
@@ -4,9 +4,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+var items = [];
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", {
+    day: day,
+    items: items,
+  });
+});
+
+app.post("/", (req, res) => {
+  items.push(req.body.todo);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
