987cdb561ec7c20bc91ca53eb9f56914274f91dc
zakk <muhammadzakaria616@gmail.com>
2022-07-24 23:53:57 +0500

    implemented mongodb

--- app.js
+++ app.js
@@ -2,13 +2,21 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var items = [];
+mongoose.connect("mongodb://localhost:27017/todoListDB", {
+  useNewUrlParser: true,
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
 
 app.get("/", (req, res) => {
   var today = new Date();
@@ -17,16 +25,39 @@ app.get("/", (req, res) => {
     day: "numeric",
     month: "long",
   };
-
   var day = today.toLocaleDateString("en-US", options);
-  res.render("list", {
-    day: day,
-    items: items,
+
+  Item.find({}, function (err, foundItems) {
+    res.render("list", {
+      day: day,
+      items: foundItems,
+    });
+    console.log("Found items in DB.");
   });
 });
 
 app.post("/", (req, res) => {
-  items.push(req.body.todo);
+  Item.create({ name: req.body.todo }, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully added new item to DB.");
+    }
+  });
+
+  res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+  const item_id = req.body.checkbox;
+  Item.deleteOne({ _id: item_id }, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully deleted item from DB.");
+    }
+  }
+  );
   res.redirect("/");
 });
 
