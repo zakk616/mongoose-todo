86d00ca48317fe5bdcdacc5677d2625c2622cfdf
zakk <muhammadzakaria616@gmail.com>
2022-07-25 22:38:56 +0500

    implemented mongo altas

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todoListDB", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  "mongodb+srv://zakk616:test_123@cluster0.9h4s5uq.mongodb.net/todoListDB",
+  { useNewUrlParser: true }
+);
 
 const itemsSchema = new mongoose.Schema({
   name: String,
@@ -18,21 +19,43 @@ const itemsSchema = new mongoose.Schema({
 
 const Item = mongoose.model("Item", itemsSchema);
 
-app.get("/", (req, res) => {
+// const item1 = new Item({
+//   name: "Welcome to your todolist!",
+// });
+
+// Item.insertMany([item1], function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved to database");
+//   }
+// });
+
+function today() {
   var today = new Date();
   var options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
-  var day = today.toLocaleDateString("en-US", options);
+  return today.toLocaleDateString("en-US", options);
+}
 
+app.get("/", (req, res) => {
   Item.find({}, function (err, foundItems) {
-    res.render("list", {
-      day: day,
-      items: foundItems,
-    });
-    console.log("Found items in DB.");
+    if (foundItems.length === 0) {
+      console.log("No items found");
+      res.render("list", {
+        items: [],
+        day: today(),
+      });
+    } else {
+      console.log("Items Found");
+      res.render("list", {
+        items: foundItems,
+        day: today(),
+      });
+    }
   });
 });
 
@@ -40,12 +63,15 @@ app.post("/", (req, res) => {
   Item.create({ name: req.body.todo }, function (err) {
     if (err) {
       console.log(err);
+      res.render("list", {
+        items: [],
+        day: today(),
+      });
     } else {
       console.log("Successfully added new item to DB.");
+      res.redirect("/");
     }
   });
-
-  res.redirect("/");
 });
 
 app.post("/delete", (req, res) => {
@@ -56,11 +82,16 @@ app.post("/delete", (req, res) => {
     } else {
       console.log("Successfully deleted item from DB.");
     }
-  }
-  );
+  });
   res.redirect("/");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
+// app.listen(3000, () => {
+//   console.log("server is running at Local port: 3000!");
+// });
+
+// this code lest app run on heroku
+function run(port) {
+  console.log("server is running at port: " + port);
+}
+app.listen(process.env.PORT, run(process.env.PORT));
